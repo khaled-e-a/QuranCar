0c8dfd7b239e4f83be87ba52b7c5071a5ffe591e
Khaled E. Ahmed <k.e.elsayed@gmail.com>
2025-02-04 20:10:41 -0500

    Initial Commit

---
+++ QuranCar/QuranCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranCarApp.swift
+//  QuranCar
+//
+//  Created by Khaled Ahmed on 2025-02-04.
+//
+
+import SwiftUI
+
+@main
+struct QuranCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
